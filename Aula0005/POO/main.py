1888f95f68f6afd58bf45bfa008b4611f2575514
Maykon D Granemann <mdg@zuplae.com>
2024-01-30 18:59:10 -0300

    Aula0005 -POO

--- Aula0005/POO/main.py
+++ Aula0005/POO/main.py
@@ -1,13 +1,9 @@
-#usuario = {'nome': nome, 'sobrenome':sobrenome, 'idade':idade}
+from usuario import Usuario
 
-class Usuario:
-    nome = ''
-    sobrenome = ''
-    idade = 0
-
-u = Usuario()
+u = Usuario() # objeto -> POO - Programacao orientada a objetos
 u.nome = 'Jorgin'
 u.sobrenome = 'Loka'
 u.idade = 69
 
+print(u)
 print(u.nome)
---
+++ Aula0005/POO/usuario.py
@@ -0,0 +1,5 @@
+class Usuario:
+    # as variaveis dentro da classe chamam atributos
+    nome = ''
+    sobrenome = ''
+    idade = 0
